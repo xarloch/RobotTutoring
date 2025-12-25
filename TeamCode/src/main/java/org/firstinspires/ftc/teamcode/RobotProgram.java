cb234a3895220451d6ec551b4f6979cdad716fe8
xarloch <mperry0507@gmail.com>
2025-12-25 13:33:00 -0500

    Made RobotProgram.java to start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotProgram.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class RobotProgram extends LinearOpMode
+{
+    DcMotor frontleft;
+    DcMotor frontright;
+    DcMotor backleft;
+    DcMotor backright;
+
+    DcMotor flywheel;
+    DcMotor lauchAngle;
+
+    Servo grabber;
+
+    @Override
+    public void runOpMode()
+    {
+        // initialize code
+        frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+        frontright = hardwareMap.get(DcMotor.class, "frontright");
+        backleft = hardwareMap.get(DcMotor.class, "backleft");
+        backright = hardwareMap.get(DcMotor.class, "backright");
+
+        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+        lauchAngle = hardwareMap.get(DcMotor.class, "launchAngle");
+
+        grabber = hardwareMap.get(Servo.class, "grabber");
+
+        waitForStart();
+
+        if (opModeIsActive())
+        {
+            while (opModeIsActive())
+            {
+
+            }
+        }
+    }
+}
