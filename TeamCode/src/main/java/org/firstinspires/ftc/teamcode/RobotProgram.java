6c4a74a24d1423e926308fd76dfb303db360bef5
xarloch <mperry0507@gmail.com>
2025-12-25 14:38:19 -0500

    Made RobotProgram.java to start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotProgram.java
@@ -12,7 +12,7 @@ public class RobotProgram extends LinearOpMode
     DcMotor backright;
 
     DcMotor flywheel;
-    DcMotor lauchAngle;
+    DcMotor launchAngle;
 
     Servo grabber;
 
@@ -26,9 +26,12 @@ public class RobotProgram extends LinearOpMode
         backright = hardwareMap.get(DcMotor.class, "backright");
 
         flywheel = hardwareMap.get(DcMotor.class, "flywheel");
-        lauchAngle = hardwareMap.get(DcMotor.class, "launchAngle");
+        launchAngle = hardwareMap.get(DcMotor.class, "launchAngle");
 
         grabber = hardwareMap.get(Servo.class, "grabber");
+
+        int grabberPos = 0;
+        boolean canChangeGrabber = true;
 
         waitForStart();
 
@@ -36,7 +39,6 @@ public class RobotProgram extends LinearOpMode
         {
             while (opModeIsActive())
             {
-
                 // drive code
                 double ly, lx, rx;
                 ly = gamepad1.left_stick_y;
@@ -47,6 +49,61 @@ public class RobotProgram extends LinearOpMode
                 frontright.setPower(ly - rx - lx);
                 backleft.setPower(  ly + rx - lx);
                 backright.setPower( ly - rx + lx);
+
+                // lauching code
+                double rt, lt;
+                rt = gamepad2.right_trigger;
+                lt = gamepad2.left_trigger;
+
+                flywheel.setPower(rt - lt);
+
+                // launch angle
+                launchAngle.setPower(gamepad2.right_stick_y);
+
+                // grabber
+                // tell the servo how to move
+                switch (grabberPos)
+                {
+                    case 0: // intake
+                    {
+                        grabber.setPosition(0.0);
+                        break;
+                    }
+
+                    case 1: // hold
+                    {
+                        grabber.setPosition(0.5);
+                        break;
+                    }
+
+                    case 2: // fire
+                    {
+                        grabber.setPosition(1.0);
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+
+                if (gamepad2.dpad_up || gamepad2.dpad_down)
+                {
+                    if (gamepad2.dpad_up && grabberPos < 2 && canChangeGrabber)
+                    {
+                        grabberPos += 1;
+                    }
+
+                    if (gamepad2.dpad_down && grabberPos > 0 && canChangeGrabber)
+                    {
+                        grabberPos -= 1;
+                    }
+
+                    canChangeGrabber = false;
+                }
+                else
+                {
+                    canChangeGrabber = true;
+                }
             }
         }
     }
